5b997e1a9279714b2089867d4e04937ae6456a58
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-07-14 01:33:23 +0100

    Initial commit

---
+++ Runtime/Scripts/ParticleGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pippinmole.Particles {
+    public class ParticleGroup : MonoBehaviour {
+
+        [SerializeField] private List<ParticleSystem> particleSystems = new List<ParticleSystem>();
+
+        public void EnableEmission(int index) {
+            if (particleSystems.Count <= 0 || index < 0) return;
+            if (index >= particleSystems.Count) throw new ArgumentOutOfRangeException($"index ({index}) cannot be larger than ParticleSystem list size ({particleSystems.Count}).");
+
+            var emission = this.particleSystems[index].emission;
+            emission.enabled = true;
+        }
+        public void EnableEmissions() {
+            for (int i = 0; i < this.particleSystems.Capacity; i++) {
+                this.EnableEmission(i);
+            }
+        }
+
+        public void DisableEmission(int index) {
+            if (particleSystems.Count <= 0 || index < 0) return;
+            if (index >= particleSystems.Count) throw new ArgumentOutOfRangeException($"index ({index}) cannot be larger than ParticleSystem list size ({particleSystems.Count}).");
+
+            var emission = this.particleSystems[index].emission;
+            emission.enabled = false;
+        }
+        public void DisableEmissions() {
+            for (int i = 0; i < this.particleSystems.Capacity; i++) {
+                this.DisableEmission(i);
+            }
+        }
+    }
+}
